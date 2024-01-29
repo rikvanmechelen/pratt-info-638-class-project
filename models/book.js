e8ceead398b5fc1d6be01ac05d6041b98a56909f
Rik Vanmechelen <rik_vanmechelen@moma.org>
2024-01-29 11:41:53 -0500

    Added upsert to books

--- models/book.js
+++ models/book.js
@@ -7,4 +7,20 @@ exports.add = (book) => {
   books.push(book);
 }
 
+exports.get = (idx) => {
+  return books[idx];
+}
+
+exports.update = (book) => {
+  books[book.id] = book;
+}
+
+exports.upsert = (book) => {
+  if (book.id) {
+    exports.update(book);
+  } else {
+    exports.add(book);
+  }
+}
+
 exports.all = books
--- routes/books.js
+++ routes/books.js
@@ -11,9 +11,15 @@ router.get('/form', async (req, res, next) => {
   res.render('books/form', { title: 'BookedIn || Books' });
 });
 
-router.post('/create', async (req, res, next) => {
+router.get('/edit', async (req, res, next) => {
+  let bookIndex = req.query.id;
+  let book = Book.get(bookIndex);
+  res.render('books/form', { title: 'BookedIn || Books', book: book, bookIndex: bookIndex });
+});
+
+router.post('/upsert', async (req, res, next) => {
   console.log('body: ' + JSON.stringify(req.body))
-  Book.add(req.body);
+  Book.upsert(req.body);
   res.redirect(303, '/books')
 });
 
