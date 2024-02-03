2708311df172ae1ead70a0d957081504971d4a4c
Rik Vanmechelen <rik_vanmechelen@moma.org>
2024-02-03 09:46:29 -0500

    added flash message to authors

--- routes/authors.js
+++ routes/authors.js
@@ -20,6 +20,12 @@ router.get('/edit', async (req, res, next) => {
 router.post('/upsert', async (req, res, next) => {
   console.log('body: ' + JSON.stringify(req.body));
   Author.upsert(req.body);
+  let createdOrupdated = req.body.id ? 'updated' : 'created';
+  req.session.flash = {
+    type: 'info',
+    intro: 'Success!',
+    message: `the author has been ${createdOrupdated}!`,
+  };
   res.redirect(303, '/authors');
 });
 
